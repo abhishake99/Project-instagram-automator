36c3fd5cabf9d70465426ea49805703d9ff0c392
ABHISHEK RAMAWAT <90638230+abhishake99@users.noreply.github.com>
2024-05-13 12:27:35 +0530

    Update selenium_code_template.py

--- selenium_code_template.py
+++ selenium_code_template.py
@@ -23,9 +23,24 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from datetime import datetime
+import undetected_chromedriver as uc
+warnings.filterwarnings("ignore")
 
+def driver_initialisation_translation():
+    service = Service(executable_path='C:/chromedriver.exe')
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--proxy-server={}'.format(curr_proxy))
+    prefs = {
+        "translate_whitelists": {"id":"en"},
+        "translate":{"enabled":"true"}
+    }
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(service=service, options=options)
+    # wait = WebDriverWait(driver,10)
+    time.sleep(15)
+    return driver
 
-def driver_initialisation():
+def driver_initialisation_normal():
     service = Service(executable_path='C:/chromedriver.exe')
     options = webdriver.ChromeOptions()
     # options.add_argument('--proxy-server={}'.format(curr_proxy))
@@ -33,4 +48,13 @@ def driver_initialisation():
     wait = WebDriverWait(driver,10)
     return driver
 
-driver=driver_initialisation()
+def driver_initialisation_undetected():
+    
+    options = uc.ChromeOptions()
+    prefs = {"profile.default_content_settings.geolocation": 2}
+    options.add_experimental_option("prefs", prefs)
+    options.add_argument("--deny-permission-prompts")
+    driver = uc.Chrome(options=options)
+    return driver
+
+driver=driver_initialisation_normal()
